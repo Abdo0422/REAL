3629ee414ac00174271e183ef210bb2c20cdeff4
Abdo0422 <119792655+Abdo0422@users.noreply.github.com>
2023-03-04 19:52:10 +0100

    first commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+
+class AdminController extends Controller
+{
+    public function view_categories()
+    {
+        $data =category::all();
+      return view('admin.category',compact('data'));
+    }
+    public function add_category(Request $request)
+    {
+        $data = new category;
+        $data->category_name=$request->catagory;
+        $data->save();
+        return redirect()->back()->with("message","Category Added Successfully");
+
+    }
+    public function delete_category($id)
+    {
+        $data =category::find($id);
+        $data->delete();
+        return redirect()->back()->with("message0","Category Deleted Successfully ");
+    }
+}
